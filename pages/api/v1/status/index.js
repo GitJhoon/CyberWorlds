67020eff8097e6e505ed64f2c23e74398f74b91b
GitJhoon <jonathan22office@gmail.com>
2025-12-02 12:10:29 -0300

    adiciona ´.env´ e ´database.js´

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,11 @@
+import database from "../../../../infra/database.js";
 
-function status(request, response){
-	response.status(200).json({"Status":"Sucesso"});
+async function status(request, response) {
+	
+	const result = await database.query('SELECT  1 + 1;');
+	console.log(result);
+
+	response.status(200).json({ Status: "Sucesso" });
 }
 
 export default status;
