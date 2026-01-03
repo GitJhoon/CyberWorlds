f3871d412a29ca43c88a2148c335a8c000258a50
GitJhoon <jonathan22office@gmail.com>
2026-01-02 22:03:05 -0300

    Removendo vulnerabilidade de SQL Injection e tratando erro de vazamento de conexoes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,38 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-	
-	const result = await database.query('SELECT  1 + 1;');
-	console.log(result);
+  const databaseVersionJson = await database.query("SELECT version();");
 
-	response.status(200).json({ Status: "Sucesso" });
+  const databaseVersion = databaseVersionJson.rows[0].version.split(" ")[1];
+
+  const databaseMaxConnectionsJson = await database.query(
+    "SHOW max_connections;",
+  );
+
+  const databaseMaxConnections =
+    databaseMaxConnectionsJson.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseConnectionsJson = await database.query({
+    text: "SELECT * FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseConnections = databaseConnectionsJson.rowCount;
+
+  const updateAt = new Date().toISOString();
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependecies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        openned_connections: parseInt(databaseConnections),
+      },
+    },
+  });
 }
 
 export default status;
