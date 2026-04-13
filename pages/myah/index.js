990b6940a5c320ceb90b681cafe4761856513b6d
GitJhoon <jonathan22office@gmail.com>
2026-04-13 15:36:30 -0300

    Adicionando mensagem

---
+++ pages/myah/index.js
@@ -0,0 +1,25 @@
+function Home() {
+  return (
+    <>
+      <h1>
+        Melhoras Myah, que você se recupere rápido e possa estar comendo
+        bastante sushi novamente kkk (apesar de eu não entender como vc gosta
+        disso) :)
+      </h1>
+      <Imagem />
+    </>
+  );
+}
+
+function Imagem() {
+  return (
+    <div>
+      <img
+        src="https://i.pinimg.com/originals/bb/f4/f3/bbf4f32f8be0b7ee2fb2773414b5c3f4.gif"
+        alt="Melhoras.gif"
+      />
+    </div>
+  );
+}
+
+export default Home;
